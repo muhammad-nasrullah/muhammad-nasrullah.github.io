bcd044515b93cdbf773c3fce678db335a5fc2d9c
muhammad-nasrullah <nhazruel.muhammad@gmail.com>
2023-10-30 20:19:53 +0700

    new ui

---
+++ src/constants/portfolio.ts
@@ -0,0 +1,310 @@
+import type { Portfolio } from "@/types";
+
+export const portfolioList: Portfolio[] = [
+  {
+    title: "My Personal Portfolio",
+    imageThumbnail: "/assets/images/project-01.webp",
+    description:
+      "This is a personal branding project that I created using Next JS, Tailwind CSS, and Framer Motion. This project consists of a website that showcases my skills, achievements, and personality. The website has seven sections: Home, About, Experiences, Services, Portfolio, Articles, and Contact. The website is designed to be responsive, interactive, and engaging. The website aims to impress and connect with potential clients, employers, and collaborators.",
+    livePreviewURL: "https://muhammad-sufyan-portfolio.vercel.app/",
+    repositoryURL: "https://github.com/Muhammad-Sufyan-901/personal-portfolio",
+    techStack: [
+      {
+        tech: "Next JS",
+        logo: "/assets/svg/next js.svg",
+      },
+      {
+        tech: "Tailwind",
+        logo: "/assets/svg/tailwind.svg",
+      },
+      {
+        tech: "Framer Motion",
+        logo: "/assets/svg/framer motion.svg",
+      },
+    ],
+  },
+  {
+    title: "Phantom Landing Page",
+    imageThumbnail: "/assets/images/project-02.webp",
+    description:
+      "Phantom is a website that I created using HTML, CSS, and JavaScript. This website displays information about a company that sells phones with the Phantom brand. This website also showcases the features, specifications, and prices of the Phantom phones, as well as the customer reviews and feedback. This website aims to attract and inform potential buyers about the quality and innovation of the Phantom phones.",
+    livePreviewURL: "https://lomba-web-design.vercel.app/",
+    repositoryURL: "https://github.com/Muhammad-Sufyan-901/lomba-web-design",
+    techStack: [
+      {
+        tech: "HTML",
+        logo: "/assets/svg/html.svg",
+      },
+      {
+        tech: "CSS",
+        logo: "/assets/svg/css.svg",
+      },
+      {
+        tech: "JavaScript",
+        logo: "/assets/svg/javascript.svg",
+      },
+    ],
+  },
+  {
+    title: "Petabyte Landing Page",
+    imageThumbnail: "/assets/images/project-03.webp",
+    description:
+      "Petabyte Landing Page is a website that I created using HTML, CSS, and Bootstrap. This website displays information about a software house that operates in the field of education, such as application, website, and educational game creation services. This website also has portfolio, testimonial, and consultation features that make it easy for visitors to know the quality and excellence of this software house.",
+    livePreviewURL: "https://petabyte-landing-page.vercel.app/",
+    repositoryURL: "https://github.com/Muhammad-Sufyan-901/petabyte-landing-page",
+    techStack: [
+      {
+        tech: "HTML",
+        logo: "/assets/svg/html.svg",
+      },
+      {
+        tech: "CSS",
+        logo: "/assets/svg/css.svg",
+      },
+      {
+        tech: "Bootstrap",
+        logo: "/assets/svg/bootstrap.svg",
+      },
+      {
+        tech: "JavaScript",
+        logo: "/assets/svg/javascript.svg",
+      },
+    ],
+  },
+  {
+    title: "HooBank Landing Page",
+    imageThumbnail: "/assets/images/project-04.webp",
+    description:
+      "HooBank is a website that I created using React JS, Tailwind CSS, and Vite. This website displays information about a company that operates in the field of financial digitalization, such as payment, transfer, and online investment services. This website also has security, convenience, and ease features that make it easy for users to manage their finances digitally.",
+    livePreviewURL: "https://hoobank-landing-page-rouge.vercel.app/",
+    repositoryURL: "https://github.com/Muhammad-Sufyan-901/hoobank-landing-page",
+    techStack: [
+      {
+        tech: "React JS",
+        logo: "/assets/svg/react.svg",
+      },
+      {
+        tech: "Tailwind",
+        logo: "/assets/svg/tailwind.svg",
+      },
+      {
+        tech: "Vite",
+        logo: "/assets/svg/vite.svg",
+      },
+    ],
+  },
+  {
+    title: "OurTube",
+    imageThumbnail: "/assets/images/project-05.webp",
+    description:
+      "OurTube is a website that I created using React JS and Material UI. This website is a clone of YouTube for learning purposes. This website allows users to watch, and search on videos from various categories and channels. This website also has features such as recommendations. This website aims to demonstrate my ability to create a complex and functional web application using modern technologies.",
+    livePreviewURL: "https://ourtube-ochre.vercel.app/",
+    repositoryURL: "https://github.com/Muhammad-Sufyan-901/ourtube",
+    techStack: [
+      {
+        tech: "React JS",
+        logo: "/assets/svg/react.svg",
+      },
+      {
+        tech: "Material UI",
+        logo: "/assets/svg/material ui.svg",
+      },
+    ],
+  },
+  {
+    title: "Twitter Clone",
+    imageThumbnail: "/assets/images/project-06.webp",
+    description:
+      "This is a Twitter clone that I created using Next JS, Typescript, and Tailwind CSS for learning purposes. This website allows users to create, like, and retweet posts from other users. This website also has features such as authentication. This website aims to show my proficiency in using Next JS, Typescript, and Tailwind CSS to create a dynamic and responsive web application.",
+    livePreviewURL: "https://twitter-web-clone-mauve.vercel.app/",
+    repositoryURL: "https://github.com/Muhammad-Sufyan-901/twitter-web-clone",
+    techStack: [
+      {
+        tech: "Next JS",
+        logo: "/assets/svg/next js.svg",
+      },
+      {
+        tech: "Tailwind",
+        logo: "/assets/svg/tailwind.svg",
+      },
+      {
+        tech: "TypeScript",
+        logo: "/assets/svg/typescript.svg",
+      },
+    ],
+  },
+  {
+    title: "YoDoo",
+    imageThumbnail: "/assets/images/project-07.webp",
+    description:
+      "YoDoo is a website that I created using React JS. This website is a simple to do list application. This website allows users to add, edit, delete, and mark as done their tasks. This website aims to help users manage their daily activities and goals in an easy and fun way.",
+    livePreviewURL: "https://yodoo.vercel.app/",
+    repositoryURL: "https://github.com/Muhammad-Sufyan-901/yodoo",
+    techStack: [
+      {
+        tech: "React JS",
+        logo: "/assets/svg/react.svg",
+      },
+    ],
+  },
+  {
+    title: "Crappo Landing Page",
+    imageThumbnail: "/assets/images/project-08.webp",
+    description:
+      "Crappo is a website that I created using React JS and Tailwind CSS technologies. This website is about cryptocurrency investment. This website allows users to view, buy, and sell various cryptocurrencies such as Bitcoin, Ethereum, and Dogecoin. This website also has features such as charts, news, and alerts. This website aims to provide users with a convenient and secure platform to invest in the growing and volatile cryptocurrency market.",
+    livePreviewURL: "https://crappo-landing-page-eight.vercel.app/",
+    repositoryURL: "https://github.com/Muhammad-Sufyan-901/crappo-landing-page",
+    techStack: [
+      {
+        tech: "React JS",
+        logo: "/assets/svg/react.svg",
+      },
+      {
+        tech: "Tailwind",
+        logo: "/assets/svg/tailwind.svg",
+      },
+    ],
+  },
+  {
+    title: "Geritch Landing Page",
+    imageThumbnail: "/assets/images/project-09.webp",
+    description:
+      "Geritch is a website that I created using React JS. This website is about a luxurious restaurant. This website allows users to view the menu, gallery, and reviews of the restaurant. This website also has features such as reservation, delivery, and gift card. This website aims to attract and impress customers with the high-quality and exquisite cuisine and service of the restaurant.",
+    livePreviewURL: "https://geritch-restaurant.vercel.app/",
+    repositoryURL: "https://github.com/Muhammad-Sufyan-901/geritch-restaurant",
+    techStack: [
+      {
+        tech: "React JS",
+        logo: "/assets/svg/react.svg",
+      },
+    ],
+  },
+  {
+    title: "XPense Landing Page",
+    imageThumbnail: "/assets/images/project-10.webp",
+    description:
+      "XPense is a website that I created using React JS and Tailwind CSS. This website is about a company that provides cloud server services. This website allows users to choose, customize, and manage their cloud servers according to their needs and budget. This website also has features such as billing, support, and analytics. This website aims to offer users a reliable and affordable solution to host their websites and applications on the cloud.",
+    livePreviewURL: "https://xpense-landing-page-kappa.vercel.app/",
+    repositoryURL: "https://github.com/Muhammad-Sufyan-901/xpense-landing-page",
+    techStack: [
+      {
+        tech: "React JS",
+        logo: "/assets/svg/react.svg",
+      },
+      {
+        tech: "Tailwind",
+        logo: "/assets/svg/tailwind.svg",
+      },
+    ],
+  },
+  {
+    title: "Elecar Landing Page",
+    imageThumbnail: "/assets/images/project-11.webp",
+    description:
+      "Elecar is a website that I created using HTML, CSS, and JavaScript technologies. This website is about a company that sells electric cars. This website allows users to browse, compare, and order various models of electric cars that are eco-friendly, efficient, and stylish. This website also has features such as testimonials, FAQs, and contact. This website aims to promote and sell the electric cars of the company.",
+    livePreviewURL: "https://elecar-landing-page.vercel.app/",
+    repositoryURL: "https://github.com/Muhammad-Sufyan-901/elecar-landing-page",
+    techStack: [
+      {
+        tech: "HTML",
+        logo: "/assets/svg/html.svg",
+      },
+      {
+        tech: "CSS",
+        logo: "/assets/svg/css.svg",
+      },
+      {
+        tech: "JavaScript",
+        logo: "/assets/svg/javascript.svg",
+      },
+    ],
+  },
+  {
+    title: "Coffee Landing Page",
+    imageThumbnail: "/assets/images/project-12.webp",
+    description:
+      "Coffee is a website that I created using HTML, CSS, and JavaScript. This website is about a cafe that serves delicious coffee, tea, and pastries. This website allows users to view the menu, location, and hours of the cafe. This website also has features such as online ordering, loyalty program, and feedback. This website aims to attract and satisfy customers with the quality and variety of the cafe’s products and services.",
+    livePreviewURL: "https://coffee-landing-page-xi.vercel.app/",
+    repositoryURL: "https://github.com/Muhammad-Sufyan-901/coffee-landing-page",
+    techStack: [
+      {
+        tech: "HTML",
+        logo: "/assets/svg/html.svg",
+      },
+      {
+        tech: "CSS",
+        logo: "/assets/svg/css.svg",
+      },
+      {
+        tech: "JavaScript",
+        logo: "/assets/svg/javascript.svg",
+      },
+    ],
+  },
+  {
+    title: "Holux Landing Page",
+    imageThumbnail: "/assets/images/project-13.webp",
+    description:
+      "Holux is a website that I created using HTML, CSS, and JavaScript. This website is about a real estate company that sells properties. This website allows users to browse, filter, and inquire about various properties such as houses, apartments, and lands. This website also has features such as valuation, financing, and consultation. This website aims to provide users with a professional and trustworthy service to buy or sell their properties.",
+    livePreviewURL: "https://holux-landing-page.vercel.app/",
+    repositoryURL: "https://github.com/Muhammad-Sufyan-901/holux-landing-page",
+    techStack: [
+      {
+        tech: "HTML",
+        logo: "/assets/svg/html.svg",
+      },
+      {
+        tech: "CSS",
+        logo: "/assets/svg/css.svg",
+      },
+      {
+        tech: "JavaScript",
+        logo: "/assets/svg/javascript.svg",
+      },
+    ],
+  },
+  {
+    title: "Shop Landing Page",
+    imageThumbnail: "/assets/images/project-14.webp",
+    description:
+      "Shop is a website that I created using HTML, CSS, and JavaScript. This website is about a boutique that sells fashionable clothes, accessories, and shoes. This website allows users to view, add to cart, and checkout the products that they like. This website also has features such as reviews, ratings, and discounts. This website aims to attract and delight customers with the quality and style of the boutique’s products.",
+    livePreviewURL: "https://shop-landing-page-six.vercel.app/",
+    repositoryURL: "https://github.com/Muhammad-Sufyan-901/shop-landing-page",
+    techStack: [
+      {
+        tech: "HTML",
+        logo: "/assets/svg/html.svg",
+      },
+      {
+        tech: "CSS",
+        logo: "/assets/svg/css.svg",
+      },
+      {
+        tech: "JavaScript",
+        logo: "/assets/svg/javascript.svg",
+      },
+    ],
+  },
+  {
+    title: "Berotot Landing Page",
+    imageThumbnail: "/assets/images/project-15.webp",
+    description:
+      "Berotot is a website that I created using HTML, CSS, and JavaScript technologies. This website is about a gym that offers various fitness programs, equipment, and trainers. This website allows users to view the gym’s facilities, schedule, and membership plans. This website also has features such as online booking, payment, and feedback. This website aims to attract and motivate customers with the quality and diversity of the gym’s services.",
+    livePreviewURL: "https://berotot-landing-page.vercel.app/",
+    repositoryURL: "https://github.com/Muhammad-Sufyan-901/berotot-landing-page",
+    techStack: [
+      {
+        tech: "HTML",
+        logo: "/assets/svg/html.svg",
+      },
+      {
+        tech: "CSS",
+        logo: "/assets/svg/css.svg",
+      },
+      {
+        tech: "JavaScript",
+        logo: "/assets/svg/javascript.svg",
+      },
+    ],
+  },
+];
---
+++ src/constants/profile.ts
@@ -0,0 +1,27 @@
+import type { Profile } from "@/types";
+
+export const profileData: Profile = {
+  name: "Muhammad Sufyan",
+  region: "Indonesia",
+  CV: "/assets/pdf/Muhammad Sufyan CV.pdf",
+  divisions: ["Frontend Developer", "Software Tester", "Mobile Developer"],
+  pictures: ["/assets/images/home-profile.webp", "/assets/images/about-profile.webp"],
+  descriptions: [
+    "I create stunning website for your business, Intermediate experienced in web design and development.",
+    "Hello everyone! I am Muhammad Sufyan, a frontend mobile and website developer. I am a intermediate experienced and I hope with this I can always learning something new to be able to invent digital applications that can help many people in the future. Can work alone or in a team.",
+  ],
+  experiences: [
+    {
+      number: 2,
+      title: "Years Of Experience",
+    },
+    {
+      number: 4,
+      title: "Programming Language Used",
+    },
+    {
+      number: 6,
+      title: "Successed Projects",
+    },
+  ],
+};
