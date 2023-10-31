1cc0a8ed01693df249b81d42fce779dac8478f6e
muhammad-nasrullah <nhazruel.muhammad@gmail.com>
2023-10-31 21:09:46 +0700

    change project and contact

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ function myMenuFunction(){
 
 /* ----- TYPING EFFECT ----- */
  let typingEffect = new Typed(".typedText",{
-    strings : ["Developer","Designer"],
+    strings : ["Web Developer","Web Designer"],
     loop : true,
     typeSpeed : 100, 
     backSpeed : 80,
